08daba1dd51ba24fcc0dc84264789865f5b9b06a
Meron Solomon <meronsolomondana@gmail.com>
2025-05-18 12:00:06 +0300

    Update Tailwind config and styles for responsive grid layout

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,7 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./*.html", "./src/**/*.{html,js}"], // 👈 this line is key!
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
